a43471e9c699c1202af7891557d797a4403ba38f
Ian-Woo Kim <ianwookim@gmail.com>
2026-02-08 15:33:11 -0500

    Start implot3d binding generation

--- implot3d-gen/Gen.hs
+++ implot3d-gen/Gen.hs
@@ -140,11 +140,11 @@ imVec2 =
     []
     False
 
-------------------
--- start implot --
-------------------
+--
+-- from implot
+--
 
-cabal =
+implot_cabal =
   Cabal
     { cabal_pkgname = CabalName "implot",
       cabal_version = "1.0.0.0",
@@ -162,174 +162,37 @@ cabal =
       cabal_buildType = Simple
     }
 
-imPlotContext :: Class
-imPlotContext =
-  Class
-    cabal
-    "ImPlotContext"
-    []
-    mempty
-    Nothing
-    []
-    []
-    []
-    False
+------------------
+-- start implot3d --
+------------------
 
-imPlotFlags_ :: EnumType
-imPlotFlags_ =
-  EnumType
-    { enum_name = "ImPlotFlags_",
-      enum_cases =
-        [ "ImPlotFlags_None",
-          "ImPlotFlags_NoTitle",
-          "ImPlotFlags_NoLegend",
-          "ImPlotFlags_NoMouseText",
-          "ImPlotFlags_NoInputs",
-          "ImPlotFlags_NoMenus",
-          "ImPlotFlags_NoBoxSelect",
-          "ImPlotFlags_NoChild",
-          "ImPlotFlags_NoFrame",
-          "ImPlotFlags_Equal",
-          "ImPlotFlags_Crosshairs",
-          "ImPlotFlags_CanvasOnly"
-        ],
-      enum_header = "implot.h"
+cabal =
+  Cabal
+    { cabal_pkgname = CabalName "implot3d",
+      cabal_version = "1.0.0.0",
+      cabal_cheaderprefix = "ImPlot3D",
+      cabal_moduleprefix = "ImPlot3D",
+      cabal_additional_c_incs = [],
+      cabal_additional_c_srcs = [],
+      cabal_additional_pkgdeps = [CabalName "stdcxx", CabalName "imgui", CabalName "implot"],
+      cabal_license = Just "BSD-3-Clause",
+      cabal_licensefile = Just "LICENSE",
+      cabal_extraincludedirs = [],
+      cabal_extralibdirs = [],
+      cabal_extrafiles = [],
+      cabal_pkg_config_depends = ["libimplot3d"],
+      cabal_buildType = Simple
     }
 
-imPlotAxisFlags_ :: EnumType
-imPlotAxisFlags_ =
-  EnumType
-    { enum_name = "ImPlotAxisFlags_",
-      enum_cases =
-        [ "ImPlotAxisFlags_None",
-          "ImPlotAxisFlags_NoLabel",
-          "ImPlotAxisFlags_NoGridLines",
-          "ImPlotAxisFlags_NoTickMarks",
-          "ImPlotAxisFlags_NoTickLabels",
-          "ImPlotAxisFlags_NoInitialFit",
-          "ImPlotAxisFlags_NoMenus",
-          "ImPlotAxisFlags_NoSideSwitch",
-          "ImPlotAxisFlags_NoHighlight",
-          "ImPlotAxisFlags_Opposite",
-          "ImPlotAxisFlags_Foreground",
-          "ImPlotAxisFlags_Invert",
-          "ImPlotAxisFlags_AutoFit",
-          "ImPlotAxisFlags_RangeFit",
-          "ImPlotAxisFlags_PanStretch",
-          "ImPlotAxisFlags_LockMin",
-          "ImPlotAxisFlags_LockMax",
-          "ImPlotAxisFlags_Lock",
-          "ImPlotAxisFlags_NoDecorations",
-          "ImPlotAxisFlags_AuxDefault"
-        ],
-      enum_header = "implot.h"
-    }
+classes = []
 
-imPlotStyleVar_ :: EnumType
-imPlotStyleVar_ =
-  EnumType
-    { enum_name = "ImPlotStyleVar_",
-      enum_cases =
-        [ "ImPlotStyleVar_LineWeight",
-          "ImPlotStyleVar_Marker",
-          "ImPlotStyleVar_MarkerSize",
-          "ImPlotStyleVar_MarkerWeight",
-          "ImPlotStyleVar_FillAlpha",
-          "ImPlotStyleVar_ErrorBarSize",
-          "ImPlotStyleVar_ErrorBarWeight",
-          "ImPlotStyleVar_DigitalBitHeight",
-          "ImPlotStyleVar_DigitalBitGap",
-          "ImPlotStyleVar_PlotBorderSize",
-          "ImPlotStyleVar_MinorAlpha",
-          "ImPlotStyleVar_MajorTickLen",
-          "ImPlotStyleVar_MinorTickLen",
-          "ImPlotStyleVar_MajorTickSize",
-          "ImPlotStyleVar_MinorTickSize",
-          "ImPlotStyleVar_MajorGridSize",
-          "ImPlotStyleVar_MinorGridSize",
-          "ImPlotStyleVar_PlotPadding",
-          "ImPlotStyleVar_LabelPadding",
-          "ImPlotStyleVar_LegendPadding",
-          "ImPlotStyleVar_LegendInnerPadding",
-          "ImPlotStyleVar_MousePosPadding",
-          "ImPlotStyleVar_AnnotationPadding",
-          "ImPlotStyleVar_FitPadding",
-          "ImPlotStyleVar_PlotDefaultSize",
-          "ImPlotStyleVar_PlotMinSize",
-          "ImPlotStyleVar_COUNT"
-        ],
-      enum_header = "implot.h"
-    }
+enums = []
 
-classes =
-  [ imPlotContext
-  ]
-
-enums =
-  [ imPlotFlags_,
-    imPlotAxisFlags_,
-    imPlotStyleVar_
-  ]
-
-toplevelfunctions :: [TopLevel]
-toplevelfunctions =
-  -- for now
-  [ TLOrdinary (TopLevelFunction FFIUnsafe void_ "CreateContext" [] (Just "createImPlotContext")),
-    TLOrdinary (TopLevelFunction FFIUnsafe void_ "ShowDemoWindow" [star CTBool "p_open"] (Just "showImPlotDemoWindow")),
-    TLOrdinary (TopLevelFunction FFIUnsafe bool_ "BeginPlot" [cstring "title_id", cppclassref imVec2 "size", int "flags"] Nothing),
-    TLOrdinary (TopLevelFunction FFIUnsafe bool_ "BeginPlot" [cstring "title_id"] (Just "beginPlot_")),
-    TLOrdinary (TopLevelFunction FFIUnsafe void_ "EndPlot" [] Nothing),
-    TLOrdinary (TopLevelFunction FFIUnsafe void_ "SetupAxes" [cstring "x_label", cstring "y_label", int "x_flags", int "y_flags"] Nothing),
-    -- style
-    TLOrdinary (TopLevelFunction FFIUnsafe void_ "PushStyleVar" [int "idx", cppclassref imVec2 "val"] (Just "PushStyleVar1")),
-    TLTemplate
-      ( TopLevelTemplateFunction
-          { topleveltfunc_safety = FFIUnsafe,
-            topleveltfunc_params = ["t1"],
-            topleveltfunc_ret = void_,
-            topleveltfunc_name = "plotLine",
-            topleveltfunc_oname = "ImPlot::PlotLine",
-            topleveltfunc_args =
-              [ cstring "label_id",
-                Arg (TemplateParamPointer "t1") "xs",
-                Arg (TemplateParamPointer "t1") "ys",
-                int "count"
-              ]
-          }
-      ),
-    TLTemplate
-      ( TopLevelTemplateFunction
-          { topleveltfunc_safety = FFIUnsafe,
-            topleveltfunc_params = ["t1"],
-            topleveltfunc_ret = void_,
-            topleveltfunc_name = "plotLine1",
-            topleveltfunc_oname = "ImPlot::PlotLine",
-            topleveltfunc_args =
-              [ cstring "label_id",
-                Arg (TemplateParamPointer "t1") "values",
-                int "count",
-                double "xscale",
-                double "xstart",
-                int "flags",
-                int "offset"
-              ]
-          }
-      )
-  ]
+toplevelfunctions = []
 
 templates = []
 
-headers =
-  [ ( MU_TopLevel,
-      ModuleUnitImports
-        { muimports_namespaces = ["ImPlot"],
-          muimports_headers =
-            [ "implot.h"
-            ]
-        }
-    ),
-    modImports "ImPlotContext" [] ["implot.h"]
-  ]
+headers = []
 
 extraLib = []
 
@@ -379,12 +242,12 @@ main = do
       let fficfg =
             FFICXXConfig
               { fficxxconfig_workingDir = cwd </> "tmp" </> "working",
-                fficxxconfig_installBaseDir = cwd </> "implot",
+                fficxxconfig_installBaseDir = cwd </> "implot3d",
                 fficxxconfig_staticFileDir = tmplDir
               }
           sbcfg =
             SimpleBuilderConfig
-              { sbcTopModule = "ImPlot",
+              { sbcTopModule = "ImPlot3D",
                 sbcModUnitMap = ModuleUnitMap (HM.fromList headers),
                 sbcCabal = cabal,
                 sbcClasses = classes,
--- workspace/build.sh
+++ workspace/build.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 
 ghc ../imgui-gen/Gen.hs -package optparse-applicative && \
-  ../imgui-gen/Gen gen --template=../imgui-gen/template && \
-  cabal build imgui && \
-  ghc ../implot-gen/Gen.hs -package optparse-applicative && \
-  ../implot-gen/Gen gen --template=../implot-gen/template && \
-  cabal build implot && \
-  cabal exec -- ghc demo-imgui-builtin.hs -framework OpenGL -lglfw -lGL -package extra && \
-  cabal exec -- ghc demo-implot-builtin.hs -framework OpenGL -lglfw -lGL -package extra && \
-  ./demo-imgui-builtin && \
-  ./demo-implot-builtin && \
-  cabal run draw-demo && \
-  cabal run plot-demo
+../imgui-gen/Gen gen --template=../imgui-gen/template && \
+ghc ../implot-gen/Gen.hs -package optparse-applicative && \
+../implot-gen/Gen gen --template=../implot-gen/template && \
+ghc ../implot3d-gen/Gen.hs -package optparse-applicative && \
+../implot3d-gen/Gen gen --template=../implot-gen/template && \
+cabal build imgui && \
+cabal build implot && \
+cabal run draw-demo && \
+cabal run plot-demo
+
+#cabal exec -- ghc demo-imgui-builtin.hs -framework OpenGL -lglfw -lGL -package extra && \
+#cabal exec -- ghc demo-implot-builtin.hs -framework OpenGL -lglfw -lGL -package extra && \
+#./demo-imgui-builtin && \
+#./demo-implot-builtin && \
